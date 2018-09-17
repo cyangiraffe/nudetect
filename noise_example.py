9183d75bc1f76678cb4ffcb8f2d547816bb2ec2e
Julian Sanders <jmsander@caltech.edu>
2018-09-17 14:50:56 -0700

    Minor fix in noise_example.py

--- noise_example.py
+++ noise_example.py
@@ -5,7 +5,7 @@ from nudetect import Noise
 noise = Noise('/disk/lif2/spike/detectorData/'
     'H117/20180803_H117_noise_-10C_-500V/20180803_H117_noise_-10C_-500V.fits',
     data_dir='outputs/{}/noise/data',
-    plot_dir='plots/{}/noise/plots',
+    plot_dir='outputs/{}/noise/plots',
     detector='H117',
     voltage=-500,
     temp=5)
